32c14ff38f4e681488d124fb49e5f517ad7a0d20
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-08-05 16:37:22 -0300

    Portfolio

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,38 +1,99 @@
 import React from 'react';
 // import sections
 import Image from '../components/elements/Image';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../utils/SectionProps';
 
-const Portfolio = () => {
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const Portfolio = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
 
   return (
-    <div className="container-sm" style={{ paddingTop: 120 }}>
-      <div className="hero-content">
-        <h1 className="mt-10 mb-16 reveal-from-bottom text-color-primary" data-reveal-delay="200">
-          Portfólio
-        </h1>
-        <div className="container-sm">
-          <div className="reveal-from-bottom center-content" data-reveal-delay="200" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+    <div >
+      <div className="container" style={{ paddingTop: 120 }}>
+        <div className="hero-content">
+          <h2 className="mt-10 mb-16 reveal-from-bottom text-color-primary" data-reveal-delay="200">
+            Portfólio
+          </h2>
+          <div className="container" >
+            <div className="reveal-from-bottom" data-reveal-delay="200" style={{ display: 'flex', flexWrap:'wrap', justifyContent:'space-between'}}>
 
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <h3 className="mt-0 mb-12 text-color-primary">
-                  Tour Virtual
-                </h3>
-                <p className="m-0">
-                  Recurso interativo que permite aos visitantes do seu site visualizarem fotos em 360 graus do seu negócio e que pode ser personalizada com recursos de sons,  textos e links.
-                </p>
+              <div className="split-item" style={{margin: 10}}>
+                <a href="#" data-reveal-container=".split-item" >
+                  <Image
+                    src={require('../assets/images/EmaCapa.jpg')}
+                    alt="Features split 01"
+                    width={300}
+                    height={200} />
+                </a>
               </div>
-              <div data-reveal-container=".split-item">
-                <Image
-                  src={require('../assets/images/jm.jpg')}
-                  alt="Features split 01"
-                  width={528}
-                  height={396} />
+
+              <div className="split-item" style={{margin: 10}}>
+                <a href="#" data-reveal-container=".split-item" >
+                  <Image
+                    src={require('../assets/images/MatutoCapa.jpg')}
+                    alt="Features split 01"
+                    width={300}
+                    height={200} />
+                </a>
               </div>
+              <div className="split-item" style={{margin: 10}}>
+                <a href="#" data-reveal-container=".split-item" >
+                  <Image
+                    src={require('../assets/images/DiaDCapa.jpg')}
+                    alt="Features split 01"
+                    width={300}
+                    height={200} />
+                </a>
+              </div>
+              <div className="split-item" style={{margin: 10}}>
+                <a href="#" data-reveal-container=".split-item" >
+                  <Image
+                    src={require('../assets/images/EcmoCapa.jpg')}
+                    alt="Features split 01"
+                    width={300}
+                    height={200} />
+                </a>
+              </div>
+              <div className="split-item" style={{margin: 10}}>
+                <a href="#" data-reveal-container=".split-item" >
+                  <Image
+                    src={require('../assets/images/BodeReiCapa.jpg')}
+                    alt="Features split 01"
+                    width={300}
+                    height={200} />
+                </a>
+              </div>
+
+
+
             </div>
-
-
-
           </div>
         </div>
       </div>
